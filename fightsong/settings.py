cf914f79bdbb9b949d064669b582b490d119a239
zws3mb <zws3mb@virginia.edu>
2015-02-15 22:49:38 -0500

    db url set for heroku

--- fightsong/settings.py
+++ fightsong/settings.py
@@ -57,21 +57,21 @@ WSGI_APPLICATION = 'fightsong.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 #
-DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fightsong', # CREATE DATABASE heroku_example CHARACTER SET utf8 COLLATE utf8_general_ci;
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 # DATABASES = {
-#     'default': {
+#       'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'fightsong', # CREATE DATABASE heroku_example CHARACTER SET utf8 COLLATE utf8_general_ci;
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    }
+}
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -104,3 +104,16 @@ EMAIL_HOST_PASSWORD = 'alxsaddtuecclrks'
 # # Allow all host hosts/domain names for this site
 # ALLOWED_HOSTS = ['*']
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+
+#DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# try to load local_settings.py if it exists
+try:
+  from local_settings import *
+except Exception as e:
+  pass
